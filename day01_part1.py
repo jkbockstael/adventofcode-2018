c02b14e4f6fe3479589374308bb637f0316f57a5
Jean-Karim Bockstael <jkb@jkbockstael.be>
2018-12-05 14:06:54 +0100

    Solution for Day 1 part 1

---
+++ day01_part1.py
@@ -0,0 +1,7 @@
+# Advent of Code 2018 - Day 1 - Chronal Calibration
+# https://adventofcode.com/2018/day/1
+
+import sys
+
+numbers = [int(line.strip()) for line in sys.stdin.readlines()]
+print(sum(numbers))
