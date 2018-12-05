b1fc182c89890c0cfd616b819a8d0089ff73f535
Jean-Karim Bockstael <jkb@jkbockstael.be>
2018-12-05 17:53:12 +0100

    Fixed comment for Day 2 part 2

--- day02_part2.py
+++ day02_part2.py
@@ -1,4 +1,4 @@
-# Advent of Code 2018 - Day 2 - Inventory Management System
+# Advent of Code 2018 - Day 2 - Inventory Management System - Part 2
 # https://adventofcode.com/2018/day/2
 
 import sys
